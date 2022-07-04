38bde71c9fa7f343c6da1a9b9532da9ecc22101c
Ian.wu <plok74122@gmail.com>
2022-07-04 11:58:24 +0800

    init basic routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "welcome/say_hello" => "welcome#say"
+  get "welcome" => "welcome#index"
+
+  root :to => "welcome#index"
 end
