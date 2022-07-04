40902358e48010206180477c939c52f18092f885
plok74122 <plok74122@gmail.com>
2022-07-04 16:28:45 +0800

    edit & update

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -15,4 +15,17 @@ class PostsController < ApplicationController
       render :new
     end
   end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(params.require(:post).permit(:title, :content))
+      redirect_to posts_path
+    else
+      render :edit
+    end
+  end
 end
