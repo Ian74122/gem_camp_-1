714a8ccfd7a3d98287fc8015e528cb0dd0a78dc4
plok74122 <plok74122@gmail.com>
2022-07-04 16:22:27 +0800

    new & create

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,17 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(params.require(:post).permit(:title, :content))
+    if @post.save
+      redirect_to posts_path
+    else
+      render :new
+    end
+  end
 end
