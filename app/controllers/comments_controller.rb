201c904b0c40a0904916b179541f8c037652b364
plok74122 <plok74122@gmail.com>
2022-07-05 14:55:00 +0800

    comment new & create action

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,11 +5,27 @@ class CommentsController < ApplicationController
     @comments = @post.comments
   end
 
+  def new
+    @comment = Comment.new
+  end
 
+  def create
+    @comment = Comment.new(comment_params)
+    @comment.post = @post
+    if @comment.save
+      redirect_to post_comments_path(@post)
+    else
+      render :new
+    end
+  end
 
   private
 
   def set_post
     @post = Post.find params[:post_id]
+  end
+
+  def comment_params
+    params.require(:comment).permit(:content)
   end
 end
