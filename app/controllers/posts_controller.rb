f4c7f0200129a24ad4477eb4322017981b91b2cd
plok74122 <plok74122@gmail.com>
2022-07-13 11:48:35 +0800

    set up client page and index page posts controller need loging

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,9 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!, except: :index
+  before_action :authenticate_user!
   before_action :set_post, only: :show
   before_action :set_own_post, only: [:edit, :update, :destroy]
   def index
-    # @posts = Post.published.includes(:user, :categories).page(params[:page]).per(5)
-    @posts = Post.includes(:user, :categories).page(params[:page]).per(5)
+    @posts = current_user.posts.includes(:user, :categories).page(params[:page]).per(5)
   end
 
   def new
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,6 +4,7 @@ class WelcomeController < ApplicationController
   end
 
   def index
-
+    @posts = Post.published.includes(:user, :categories).page(params[:page]).per(5)
+    render 'posts/index'
   end
 end
