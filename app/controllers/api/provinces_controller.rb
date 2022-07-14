6301dbea94ca7c1f856cc559bf632ea2a250591d
plok74122 <plok74122@gmail.com>
2022-07-14 11:21:20 +0800

    fix N+1 query

--- app/controllers/api/provinces_controller.rb
+++ app/controllers/api/provinces_controller.rb
@@ -1,6 +1,6 @@
 class Api::ProvincesController < ApplicationController
   def index
-    provinces = Province.all
+    provinces = Province.eager_load(:region).all
     render json: provinces, each_serializer: ProvinceSerializer
   end
 end
--- app/serializers/province_serializer.rb
+++ app/serializers/province_serializer.rb
@@ -1,7 +1,4 @@
 class ProvinceSerializer < ActiveModel::Serializer
   attributes :code , :name, :region
-
-  def region
-    RegionSerializer.new(object.region)
-  end
+  belongs_to :region, serializer: RegionSerializer
 end
