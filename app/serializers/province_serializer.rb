778669f3fb68fea63099a72bdfad81ea9afa3585
plok74122 <plok74122@gmail.com>
2022-07-14 11:04:52 +0800

    add region information in provinces api

--- app/serializers/province_serializer.rb
+++ app/serializers/province_serializer.rb
@@ -1,3 +1,7 @@
 class ProvinceSerializer < ActiveModel::Serializer
-  attributes :code , :name, :region_id
+  attributes :code , :name, :region
+
+  def region
+    RegionSerializer.new(object.region)
+  end
 end
