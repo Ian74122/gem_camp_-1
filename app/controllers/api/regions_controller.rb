fa7fbbe57e8197b4411345097fc2c920ada7446c
plok74122 <plok74122@gmail.com>
2022-07-14 10:29:07 +0800

    build regions api v1 format

--- app/controllers/api/regions_controller.rb
+++ app/controllers/api/regions_controller.rb
@@ -2,5 +2,9 @@ class Api::RegionsController < ApplicationController
 
   def index
     @regions = Region.all
+    respond_to do |format|
+      format.html
+      format.json { render json: @regions.to_json(except: [:id, :created_at, :updated_at]) }
+    end
   end
 end
