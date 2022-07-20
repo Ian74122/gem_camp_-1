a76a7c907752a470e9f3650e3d21d60024ede69e
plok74122 <plok74122@gmail.com>
2022-07-20 12:48:36 +0800

    generate serial number

--- app/models/post.rb
+++ app/models/post.rb
@@ -10,7 +10,7 @@ class Post < ApplicationRecord
 
   mount_uploader :image, ImageUploader
 
-  after_create :generate_serial_number
+  after_commit :generate_serial_number
 
   include AASM
   aasm column: :state do
@@ -31,7 +31,12 @@ class Post < ApplicationRecord
   end
 
   def generate_serial_number
-    count_post_today = Post.where(created_at: DateTime.current.beginning_of_day..DateTime.current.end_of_day).count
-    self.update(serial_number: "#{Date.current.strftime('%y%m%d')}#{count_post_today.to_s.rjust(4, '0')}")
+    # count_post_today = Post.where(created_at: DateTime.current.beginning_of_day..DateTime.current.end_of_day).count
+    # self.update(serial_number: "#{Date.current.strftime('%y%m%d')}#{count_post_today.to_s.rjust(4, '0')}")
+    ActiveRecord::Base.connection.execute("UPDATE `posts` SET `posts`.`serial_number` = CONCAT(DATE_FORMAT(CONVERT_TZ(posts.created_at, '+00:00', '+8:00'), '%y%m%d'),
+                                                      (SELECT LPAD(count(*), 6, 0)
+                                                       FROM `posts` where `posts`.`id` <= #{id} and DATE(CONVERT_TZ(posts.created_at, '+00:00', '+8:00')) = (select DATE(CONVERT_TZ(posts.created_at, '+00:00', '+8:00'))
+                                                       FROM posts WHERE posts.id = #{id})))
+                                                       WHERE posts.id = #{id}")
   end
 end
