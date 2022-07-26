bd792286da47bfaff2ede8db17bb686a10aec72c
plok74122 <plok74122@gmail.com>
2022-07-26 11:35:02 +0800

    before update , reload geo location in user table

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable, :trackable
 
   has_many :posts
+  after_update :reload_geo_location!, if: :last_sign_in_ip_changed?
 
   def client?
     role == 'client'
@@ -12,5 +13,12 @@ class User < ApplicationRecord
 
   def admin?
     role == 'admin'
+  end
+
+  def reload_geo_location!
+    service = IpgeolocationIoService.new
+    result = service.get_location(last_sign_in_ip)
+    self.update_column(:country, result['country'])
+    self.update_column(:city, result['city'])
   end
 end
