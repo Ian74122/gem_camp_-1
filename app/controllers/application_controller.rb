84d78327e80b92aa66f43bffbd70dd21d19570c7
plok74122 <plok74122@gmail.com>
2022-09-13 11:13:23 +0800

    edit code in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :set_locale
+  include Pundit
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
   def set_locale
     if params[:locale] && I18n.available_locales.include?( params[:locale].to_sym )
@@ -7,5 +9,17 @@ class ApplicationController < ActionController::Base
     end
 
     I18n.locale = session[:locale] || I18n.default_locale
+  end
+
+  private
+
+  def user_not_authorized(exception)
+    policy_name = exception.policy.class.to_s.underscore
+    flash[:alert] = t "#{policy_name}.#{exception.query}", scope: 'pundit', default: :default,
+                      username: exception.policy.record&.user&.username
+    redirect_to(request.referrer || root_path)
+  end
+  def pundit_user
+    current_user
   end
 end
